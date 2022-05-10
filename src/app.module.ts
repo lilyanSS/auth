3809a9e71434c54f670ba1cf4ee0139747c37b7a
Lilyan Isabel <lilyansica14@gmail.com>
2022-05-09 20:28:06 -0600

    add guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { APP_GUARD } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import configuration from './config/configuration';
 import { UsersModule } from './schemas/users/user.module';
 import { AuthModule } from './schemas/auth/auth.module';
 import { RoleModule } from './schemas/role/role.module';
+import { GqlAuthGuard} from './schemas/auth/guards/jwt-auth.guard';
+
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -22,7 +25,12 @@ import { RoleModule } from './schemas/role/role.module';
     forwardRef(() => RoleModule),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_GUARD,
+      useClass: GqlAuthGuard,
+    },
+  ],
   exports: [TypeOrmModule]
 })
 
